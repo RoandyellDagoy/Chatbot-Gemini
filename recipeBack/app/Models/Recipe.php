34eda43984711e27f83382f988ce49c43333c958
Dagoy, Roandyell Khen <118450581+RoandyellDagoy@users.noreply.github.com>
2024-05-31 00:31:09 +0800

    backEnd

---
+++ recipeBack/app/Models/Recipe.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+
+class Recipe extends Model
+{
+    use HasFactory;
+    protected $table = 'recipes';
+    protected $fillable  =['id','recipe_name','ingredients', 'instructions','prep_time','cook_time'];
+    
+}
